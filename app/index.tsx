747941a3a929dfb9404aa24265e93653e4481c8a
Eduhuu <eduardosuarez.c97@gmail.com>
2026-03-12 18:23:56 +0000

    install tailwind

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,6 @@
-import { Text, View } from "react-native";
+import { View } from "react-native";
+import "../global.css";
 
 export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Hola mundo</Text>
-    </View>
-  );
+  return <View className="flex-1 items-center justify-center bg-white"></View>;
 }
